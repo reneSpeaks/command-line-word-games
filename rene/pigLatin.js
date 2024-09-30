2f05a8f7c2fd39d49a2f53104a8cea86e3e1013c
René Weiberlenn <rene.weiberlenn@live.de>
2024-09-30 12:22:14 +0200

    fixed capitalization in pigLatin.js

--- rene/pigLatin.js
+++ rene/pigLatin.js
@@ -4,22 +4,38 @@ const args = process.argv.slice(2);
 
 let pigLatin = "";
 
+function capitalizeFirstLetter(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
 for (let word of args) {
 
     let wordArray = word.split('');
+    let firstLetter;
+    let secondLetter;
 
     if (VOWELS.includes(wordArray[0].toLowerCase())) {
         pigLatin += " " + word + "way";
     } else {
         if (CONSONANTS.includes(wordArray[1].toLowerCase())) {
-            let firstLetter = wordArray.shift();
-            let secondLetter = wordArray.shift();
-            pigLatin += " " + wordArray.toSpliced(wordArray.length, 0, firstLetter, secondLetter).join('') + "ay";
+            firstLetter = wordArray.shift();
+            secondLetter = wordArray.shift();
+
+            if (firstLetter === firstLetter.toUpperCase() && wordArray[0] !== undefined) {
+                wordArray[0] = capitalizeFirstLetter(wordArray[0]);
+            }
+
+            pigLatin += " " + wordArray.toSpliced(wordArray.length, 0, firstLetter.toLowerCase(), secondLetter).join('') + "ay";
         } else {
-            let firstLetter = wordArray.shift();
-            pigLatin += " " + wordArray.toSpliced(wordArray.length, 0, firstLetter).join('') + "ay";
+            firstLetter = wordArray.shift();
+
+            if (firstLetter === firstLetter.toUpperCase() && wordArray[0] !== undefined) {
+                wordArray[0] = capitalizeFirstLetter(wordArray[0]);
+            }
+
+            pigLatin += " " + wordArray.toSpliced(wordArray.length, 0, firstLetter.toLowerCase()).join('') + "ay";
         }
     }
 }
 
-console.log(`Output: ${pigLatin.trimStart()}`)
+console.log(`# Output: ${pigLatin.trimStart()}`)
